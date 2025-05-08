135d11a22b22beb6d1aacb6848dd5ee505c8a8d1
unknown <tasnimchowdhuryr@gmail.com>
2025-05-08 20:23:32 +0600

    Login Logout

---
+++ blogProject3/posts/views.py
@@ -0,0 +1,73 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from . import forms
+from . import models
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView, UpdateView, DeleteView
+# Create your views here.
+
+@login_required
+def add_post(request):
+    if request.method == 'POST':
+        post_form = forms.PostForm(request.POST)
+        if post_form.is_valid():
+            # post_form.cleaned_data['author'] = request.user
+            post_form.instance.author = request.user
+            post_form.save()
+            return redirect('add_post')
+    else:
+        post_form = forms.PostForm()
+    return render(request, 'add_post.html', {'form' : post_form})
+
+
+#Add post using class based view
+@method_decorator(login_required, name='dispatch')
+class AddPostCreateView(CreateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'add_post.html'
+    success_url = reverse_lazy('add_post')
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+@login_required
+def edit_post(request, id):
+    post = models.Post.objects.get(pk=id)
+    post_form = forms.PostForm(instance=post)
+    if request.method == 'POST':
+        post_form = forms.PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.instance.author = request.user
+            post_form.save()
+            return redirect('homepage')
+    
+    return render(request, 'add_post.html', {'form' : post_form})
+
+
+# post edit using class based view
+@method_decorator(login_required, name='dispatch')
+class EditPostView(UpdateView):
+    model = models.Post
+    form_class = forms.PostForm
+    template_name = 'add_post.html'
+    pk_url_kwarg = 'id'
+    success_url = reverse_lazy('profile')
+
+
+@login_required
+def delete_post(request, id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    return redirect('homepage')
+
+
+# post delete using class based view
+@method_decorator(login_required, name='dispatch')
+class DeletePostView(DeleteView):
+    model = models.Post
+    template_name = 'delete.html'
+    success_url = reverse_lazy('profile')
+    pk_url_kwarg = 'id'
